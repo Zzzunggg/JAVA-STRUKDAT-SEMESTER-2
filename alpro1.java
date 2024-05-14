0bd5943ad47d0a79d31133cd5e2c1b942a1820ae
Zzzunggg <153978802+Zzzunggg@users.noreply.github.com>
2024-05-14 22:11:43 +0700

    Add files via upload

---
+++ alpro1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class alpro1 {
+    public static void main(String[] args) {
+        // Membuat array untuk menyimpan 10 angka
+        int[] angka = new int[10];
+        // Membaca 10 angka dari pengguna
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan angka ke-" + (i+1) + ": ");
+            angka[i] = input.nextInt();
+        }
+        
+        // Menampilkan elemen pertama dan terakhir dari array
+        System.out.println("Elemen pertama: " + angka[0]);
+        System.out.println("Elemen terakhir: " + angka[9]);
+    }
+}
---
+++ alpro2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class alproizzul2 {
+    public static void main(String[] args) {
+        // Membuat array untuk menyimpan 10 angka
+        int[] angka = new int[10];
+        double total = 0;
+        
+        // Membaca 10 angka dari pengguna dan menghitung total
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan angka ke-" + (i+1) + ": ");
+            angka[i] = input.nextInt();
+            total += angka[i];
+        }
+        
+        // Menghitung rata-rata
+        double rataRata = total / 10;
+        
+        // Menampilkan rata-rata ke layar
+        System.out.println("Rata-rata: " + rataRata);
+    }
+}

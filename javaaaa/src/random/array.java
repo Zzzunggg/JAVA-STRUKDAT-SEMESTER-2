f2aeb776c51bd8119ec08e2cdd942efb23c216a2
Zzzunggg <mhaq5546@gmail.com>
2024-05-23 01:47:42 +0700

    y
    yy

--- javaaaa/src/random/array.java
+++
@@ -1,8 +0,0 @@
-package random;
-public class array {
-    public static void main(String[] args) {
-        int cth [] = new int [10];
-        cth [2] = 5;
-        System.out.println ("nilai =" + cth [2]);
-    }
-}

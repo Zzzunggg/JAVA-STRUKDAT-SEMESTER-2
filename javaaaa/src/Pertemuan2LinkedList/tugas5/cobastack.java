4c41dd1eeb3413b5b9534590c94bf017333dc4d9
Zzzunggg <mhaq5546@gmail.com>
2024-05-23 02:05:59 +0700

    e
    r

---
+++ javaaaa/src/Pertemuan2LinkedList/tugas5/cobastack.java
@@ -0,0 +1,18 @@
+package Pertemuan2LinkedList.tugas5;
+public class cobastack {
+    public static void main(String[] args) {
+    ASstack stack = new ASstack();
+    stack.push("kucing");
+    stack.push("harimau");
+    stack.push("burung");
+    stack.push("bebek");
+    
+
+    
+    System.out.println(stack.pop());
+    System.out.println(stack.pop());
+    System.out.println(stack.pop());
+    System.out.println(stack.pop());
+    }
+}
+//uhuy

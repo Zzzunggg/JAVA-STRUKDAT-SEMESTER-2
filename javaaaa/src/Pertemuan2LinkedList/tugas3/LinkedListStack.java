cd2604f26cd0f6fce67dad149d8843480f3775a9
Zzzunggg <mhaq5546@gmail.com>
2024-05-23 01:22:09 +0700

    pusing
    gpp

---
+++ javaaaa/src/Pertemuan2LinkedList/tugas3/LinkedListStack.java
@@ -0,0 +1,28 @@
+package Pertemuan2LinkedList.tugas3;
+
+public class LinkedListStack {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+
+        stack.push(2);
+        stack.push(4);
+        stack.push(6);
+      
+        stack.display();
+
+        stack.push(8); // Push pertama dengan nilai 8
+        System.out.println(stack.pop() + " popped from stack");
+     
+    
+        stack.push(8); // Push kedua dengan nilai
+        System.out.println(stack.pop() + " popped from stack");
+        
+        System.out.println("Top element is " + stack.peek());
+
+  
+       
+
+
+        stack.display();
+    }
+}

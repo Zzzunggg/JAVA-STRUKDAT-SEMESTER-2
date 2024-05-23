22568ce1b2f8beb4223f99afe57d73dd68cf5760
Zzzunggg <mhaq5546@gmail.com>
2024-05-24 02:10:29 +0700

    halah
    hilih

---
+++ javaaaa/src/Pertemuan2LinkedList/tugas1/LinkedListStack.java
@@ -0,0 +1,19 @@
+package Pertemuan2LinkedList.tugas1;
+
+public class LinkedListStack {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+
+        stack.push(5);
+        stack.push(7);
+        stack.push(9);
+        stack.push(11);
+        stack.display();
+
+        System.out.println("Top element is " + stack.peek());
+
+        System.out.println(stack.pop() + " popped from stack");
+  
+        stack.display();
+    }
+}
---
+++ javaaaa/src/Pertemuan2LinkedList/tugas1/Stack.java
@@ -0,0 +1,65 @@
+package Pertemuan2LinkedList.tugas1;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Stack {
+    private Node top;
+
+    public Stack() {
+        this.top = null;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (isEmpty()) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
+        System.out.println(data + " pushed to stack");
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        int popped = top.data;
+        top = top.next;
+        return popped;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return top.data;
+    }
+
+    public void display() {
+        Node current = top;
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return;
+        }
+        System.out.println("Stack elements are:");
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+}

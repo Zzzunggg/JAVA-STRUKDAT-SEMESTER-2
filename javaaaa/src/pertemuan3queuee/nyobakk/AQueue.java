088febcd44a97e9d15e183dd1db607c4b09127dd
Zzung-Atjah <153978802+Zzzunggg@users.noreply.github.com>
2024-06-01 18:45:28 +0700

    Update AQueue.java

--- javaaaa/src/pertemuan3queuee/nyobakk/AQueue.java
+++ javaaaa/src/pertemuan3queuee/nyobakk/AQueue.java
@@ -4,19 +4,15 @@ public class AQueue {
     public static void main(String[] args) {
         CircularQueue queue = new CircularQueue();
 
-        queue.enqueue("1");
-        queue.enqueue("4");
-        queue.enqueue("0");
-        queue.enqueue("4");
+        queue.enqueue("kucing");
+        queue.enqueue("kalem");
+        queue.enqueue("miaw");
+        queue.enqueue("reva");
 
-        queue.enqueue("9");
-        queue.enqueue("8");
-        queue.enqueue("7");
         queue.display();
-        System.out.println(queue.dequeue());
+       
         System.out.println(queue.peek());
         System.out.println(queue.dequeue());
-        queue.enqueue("7");
         
         queue.display();
     }
